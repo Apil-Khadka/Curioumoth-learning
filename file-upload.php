c25fd0c39e84c16688cabb320e29b02957ae00d0
SIlent <87198819+Apil2019@users.noreply.github.com>
2023-01-07 13:09:29 +0545

    Add files via upload

---
+++ file-upload.php
@@ -0,0 +1,58 @@
+<?php 
+
+    // Database connection
+    include("config/database.php");
+    
+    if(isset($_POST["submit"])) {
+        // Set image placement folder
+        $target_dir = "images/";
+        // Get file path
+        $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+        // Get file extension
+        $imageExt = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // Allowed file types
+        $allowd_file_ext = array("jpg", "jpeg", "png", "txt");
+
+	//$sql = "INSERT IGNORE INTO $table (name,dir) VALUES('$name','$img_dir')";
+
+        if (!file_exists($_FILES["fileUpload"]["tmp_name"])) {
+           $resMessage = array(
+               "status" => "alert-danger",
+               "message" => "Select image to upload."
+           );
+        } else if (!in_array($imageExt, $allowd_file_ext)) {
+            $resMessage = array(
+                "status" => "alert-danger",
+                "message" => "Allowed file formats .jpg, .jpeg and .png."
+            );            
+        } else if ($_FILES["fileUpload"]["size"] > 20971520000000) {
+            $resMessage = array(
+                "status" => "alert-danger",
+                "message" => "File is too large. File size should be less than 2 megabytes."
+            );
+        } else if (file_exists($target_file)) {
+            $resMessage = array(
+                "status" => "alert-danger",
+                "message" => "File already exists."
+            );
+        } else {
+            if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
+                $sql = "INSERT INTO school (img_dir) VALUES ('$target_file')";
+                $stmt = $conn->prepare($sql);
+                 if($stmt->execute()){
+                    $resMessage = array(
+                        "status" => "alert-success",
+                        "message" => "Image uploaded successfully."
+                    );                 
+                 }
+            } else {
+                $resMessage = array(
+                    "status" => "alert-danger",
+                    "message" => "Image coudn't be uploaded."
+                );
+            }
+        }
+
+    }
+
+?>
---
+++ process-form.php
@@ -0,0 +1,70 @@
+<?php
+
+// Connect to the 'information' database
+$hostname = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=information", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Database connection failed: " . $e->getMessage();
+}
+
+// Sanitize and validate form fields
+$name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+$body = filter_input(INPUT_POST, "body", FILTER_SANITIZE_STRING);
+$priority = filter_input(INPUT_POST, "priority", FILTER_SANITIZE_STRING);
+$type = filter_input(INPUT_POST, "type", FILTER_SANITIZE_STRING);
+$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOLEAN);
+
+// Validate that the 'terms' field is true
+if (!$terms) {
+    die("Terms must be accepted");
+}
+
+// Insert record into 'message' table
+$sql = "INSERT INTO message (name, body, priority, type) VALUES (?, ?, ?, ?)";
+
+$stmt = $conn->prepare($sql);
+$stmt->bindValue(1, $name, PDO::PARAM_STR);
+$stmt->bindValue(2, $body, PDO::PARAM_STR);
+$stmt->bindValue(3, $priority, PDO::PARAM_STR);
+$stmt->bindValue(4, $type, PDO::PARAM_STR);
+
+try {
+    if ($stmt->execute()) {
+        $resMessage = array(
+            "status" => "alert-success",
+            "message" => "Record saved successfully."
+        );
+    }  else {
+      $resMessage = array(
+          "status" => "alert-danger",
+          "message" => "Error saving record: " . $stmt->$error
+      );
+  }
+} catch (Exception $e) {
+  $resMessage = array(
+      "status" => "alert-danger",
+      "message" => "Error saving record: " . $e->getMessage()
+  );
+}
+
+echo json_encode($resMessage);
+
+if ($stmt->execute()) {
+  // Form submission was successful, redirect to previous page
+  header("Location: thanks.html");
+  exit;
+} else {
+  // Form submission failed, display an error message
+  echo "Error saving record: " . $stmt->$error;
+}
+
+
+
+
+?>
